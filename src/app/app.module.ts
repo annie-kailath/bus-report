4def4f00050397e799aaa666e6037a35545e49aa
annie-kailath <anniejk91@gmail.com>
2020-09-20 10:15:47 +1000

    Added bus-service-list child component
    Modified appcomponent test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { ArrivalStatusPipe } from './pipes/arrival-status/arrival-status.pipe';
+import { BusServiceListComponent } from './components/bus-service-list/bus-service-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArrivalStatusPipe
+    ArrivalStatusPipe,
+    BusServiceListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/bus-service-list/bus-service-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { BusServiceDetails } from '../../models/bus-service-details';
+@Component({
+  selector: 'app-bus-service-list',
+  templateUrl: './bus-service-list.component.html',
+  styleUrls: ['./bus-service-list.component.scss']
+})
+export class BusServiceListComponent implements OnInit {
+
+  @Input() busServiceList: BusServiceDetails;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
